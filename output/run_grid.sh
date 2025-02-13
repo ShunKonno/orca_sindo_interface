dad28418548109c923dccd645588fd6cffd7bbbd
redshun0710 <konnoshun1985@kyotounivcoop.onmicrosoft.com>
2025-02-13 23:38:50 +0900

    apply gitignore

--- output/run_grid.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-. /home/shun/SINDO/sindo/sindovars.sh
-java RunMakePES -f nh3_grid.xml >& makeGRID.out
--- output/run_qff.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-. /home/shun/SINDO/sindo/sindovars.sh
-java RunMakePES -f nh3_output.xml >& makePES.out
